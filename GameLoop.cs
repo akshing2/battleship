7bc242f1a7c049b7c80fef6702d170c2255e7f7c
Akshin Goswami <akshin@andi.health>
2022-06-25 09:55:49 +1000

    game loop init

---
+++ GameLoop.cs
@@ -0,0 +1,13 @@
+// This file is used to run the game loop
+using Ship = Ship.Ship;
+using Playerboard = PlayerBoard.PlayerBoard;
+
+namespace GameLoop
+{
+    public class GameLoop
+    {
+        private Playerboard player = new Playerboard(); // human player
+        private Playerboard opponent = new Playerboard(); // ai player
+
+    }
+}
